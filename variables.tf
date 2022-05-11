3c156920dc48541a85e7eb7823b2f20a4e75eacd
schoud100 <99477921+schoud100@users.noreply.github.com>
2022-05-11 11:54:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,7 @@
-
-##################################
-# Azure Resource Group variables #
-##################################
-
-variable "kube_resource_group_name" {
-  type        = string
-  description = "The name of an existing Resource Group"
+variable "clientid" {
+	sensitive = true
 }
 
-variable "vaccine_location" {
-  type        = string
-  description = "Define the region the Azure Key Vault should be created, you should use the Resource Group location"
-}
-
-variable "stgaccount" {
-  type        = string
-  description = "The name of Storage Account"
+variable "spnkvsecret" {
+	sensitive = true
 }
